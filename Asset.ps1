5a0215b6ce5a077101e34b8a6657b1a6e1a17500
Jonathan Robst <JRobst@mail.com>
2020-05-18 16:06:25 -0500

    Corrected $Path
    Not sure why I left the full path instead of using path variable.

--- Asset.ps1
+++ Asset.ps1
@@ -1,13 +1,13 @@
-﻿$Path = D:\results.txt
+﻿$Path = 
 
-(Get-CimInstance -ClassName Win32_ComputerSystem).name >> D:\results.txt
-(Get-CimInstance -ClassName Win32_ComputerSystem).domain >> D:\results.txt
-(Get-CimInstance -ClassName Win32_ComputerSystem).manufacturer >> D:\results.txt
-(Get-CimInstance -ClassName Win32_ComputerSystem).model >> D:\results.txt
-$env:USERNAME >> D:\results.txt
-(Get-CimInstance -ClassName Win32_OperatingSystem).caption >> D:\results.txt
-(Get-CimInstance -ClassName Win32_OperatingSystem).buildnumber >> D:\results.txt
-wmic path softwarelicensingservice get OA3xOriginalProductKey >> D:\results.txt
-echo "" >> D:\results.txt
-echo "====" >> D:\results.txt
-echo "" >> D:\results.txt
+(Get-CimInstance -ClassName Win32_ComputerSystem).name >> $Path
+(Get-CimInstance -ClassName Win32_ComputerSystem).domain >> $Path
+(Get-CimInstance -ClassName Win32_ComputerSystem).manufacturer >> $Path
+(Get-CimInstance -ClassName Win32_ComputerSystem).model >> $Path
+$env:USERNAME >> $Path
+(Get-CimInstance -ClassName Win32_OperatingSystem).caption >> $Path
+(Get-CimInstance -ClassName Win32_OperatingSystem).buildnumber >> $Path
+wmic path softwarelicensingservice get OA3xOriginalProductKey >> $Path
+echo "" >> $Path
+echo "====" >> $Path
+echo "" >> $Path
